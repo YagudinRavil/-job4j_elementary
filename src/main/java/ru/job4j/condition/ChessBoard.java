9429a9de3ef65143956b3fad959adf2a775382e5
Ravil <yagudin.ravil1996@gmail.com>
2024-03-03 17:27:50 +0300

    4.3.6. Math.abs [#373616 #484340]

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -7,7 +7,6 @@ public class ChessBoard {
                 && isValid(y1) && isValid(y2)) {
             if (Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
                 result = Math.abs(x1 - x2);
-                result = result == 0 ? Math.abs(y1 - y2) : result;
             }
         }
         return result;
